987dc6da51861a90c43ffa6260dffeb48a86a41c
simon <simblee@gmail.com>
2012-11-05 22:05:34 +0800

    Added gitignore and source file.

---
+++ src/backbone.marionette.dust.js
@@ -0,0 +1,23 @@
+// TemplateCache methods overidden to support DustJS templates.
+_.extend(Marionette.TemplateCache.prototype, {
+
+    // Templates in the DustJS cache only require the template
+    // id to retrieve the template.
+    loadTemplate: function(templateId){
+        return templateId;
+    },
+
+    // Returns a function that calls the dust.render method
+    // IMPORTANT: Must preload templates into the DustJS cache
+    // so that dust.render runs synchronously and the callback
+    // is called immediately.
+    compileTemplate: function(template){
+        return function (data) {
+            var html;
+            dust.render(template, data, function (err, out) {
+                html = out;
+            });
+            return html;
+        }
+    }
+});
