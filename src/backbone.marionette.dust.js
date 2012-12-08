4b7d2d28a4a1f5b6b6bd3d8b0ba97d2765d168bc
simon <simblee@gmail.com>
2012-12-09 00:54:40 +0800

    Simplified to override render method rather than template cache.

--- src/backbone.marionette.dust.js
+++ src/backbone.marionette.dust.js
@@ -1,40 +1,23 @@
 define([
     'underscore',
     'backbone',
-
-    // Below here don't need to be referenced, attach to
-    // other either window or backbone object
     'marionette',
-    'jquery',
     'dust',
-    'dust.helpers' // LinkedIn Helpers - remove if not using
+    'dust.helpers' // LinkedIn Helpers
 ],
-function (_, Backbone) {
+function (_) {
 
-    // TemplateCache methods overidden to support DustJS templates.
-    _.extend(Backbone.Marionette.TemplateCache.prototype, {
+    // Render a template with data. The `template` parameter is
+    // passed to the `TemplateCache` object to retrieve the
+    // template function. Override this method to provide your own
+    // custom rendering and template handling for all of Marionette.
+    Backbone.Marionette.Renderer.render = function (template, data) {
+        var html;
+        // Will run sync if templates pre-compiled into dust chache
+        dust.render(template, data, function (err, out) {
+            html = out;
+        });
+        return html;
+    };
 
-        // Templates in the DustJS cache only require the template
-        // id to retrieve the template.
-        loadTemplate: function (templateId) {
-            return templateId;
-        },
-
-        // Returns a function that calls the DustJS 'render' method.
-        // IMPORTANT: Must preload templates into the DustJS cache
-        // so that dust.render the callback is called immediately.
-        // TODO: Incorporate into the Marionette asynchronous template library
-        compileTemplate: function (template) {
-            return function (data) {
-                var html;
-                // Will run sync if templates pre-compiled into dust chache
-                dust.render(template, data, function (err, out) {
-                    html = out;
-                });
-                return html;
-            }
-        }
-    });
-
-    return Backbone.Marionette;
 });
