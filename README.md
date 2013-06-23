7c8d1a0b7a1096658688ebba9fd1e4d5cd65d8d3
Simon Blee <simblee@gmail.com>
2013-06-24 01:27:32 +0800

    Simplified source.

--- README.md
+++ README.md
@@ -5,17 +5,11 @@ DustJS templates for Marionette. Overrides the Marionette.Renderer.render
 to allow dust rendering in the normal Marionette flow of execution. Bypasses
 Marionette TemplateCache as DustJS has its own cache.
 
-## Dependencies
-* RequireJS
-* Backbone and its dependencies
-* Dust (winth LinkedIn helpers - if you want them)
-
 ## Usage
-Include as a dependency in any file using Marionette Views along with Marionette.
-MUST pre compile templates using the DustJS pre-compiler as the render method
-must run synchronously. Later versions will allow asynchronous rendering with
-Marionette Async and with assistance from a user defined dust.onLoad function
-for loading templates asynchronously.
+Templates must be compiled and in the Dust cache. Pass the template name as the
+first argument and the data object as the second argument to the
+Marionette.Renderer.render function. The rendered markup will be returned.
 
-Remove the dust.helpers module if you aren't using the LinkedIn helpers (these
-are recomended as they provide some very helpful functions inside your templates).
+For use in a View, simply set the template property on the view to the template
+name. Marionette will call Marionette.Renderer.render and insert the markup into
+the DOM.
--- src/backbone.marionette.dust.js
+++ src/backbone.marionette.dust.js
@@ -1,38 +1,17 @@
 (function (root, factory) {
   if (typeof exports === 'object') {
-
-    var jquery = require('jquery'),
-        underscore = require('underscore'),
-        backbone = require('backbone'),
-        marionette = require('marionette')
-        dust = require('dust'),
-        dustHelpers = require('dust.helpers');
-
-    module.exports = factory(jquery, underscore, backbone, marionette, dust, dustHelpers);
-
+    var backbone = require('backbone'),
+        dust = require('dust');
+    module.exports = factory(backbone, dust);
   } else if (typeof define === 'function' && define.amd) {
-
-    define(['jquery', 'underscore', 'backbone', 'marionette', 'dust', 'dust.helpers'], factory);
-
+    define(['backbone'], factory);
   }
-}(this, function ($, _, Backbone) {
+}(this, function (Backbone, dust) {
 
-    // IMPORTANT: Must preload templates into the DustJS cache
-    // so that the dust.render callback is called immediately.
-    // DustJS has its own cache so the Marionette cache is not
-    // required. Templates can be precompiled and loaded as a
-    // single script or compiled in the browser when the application
-    // boots.
-
-    // TODO: Incorporate into the Marionette asynchronous
-    // template library. Then a dust.onLoad function can
-    // be defined to load templates dynamically from your
-    // the filesystem or database. If on the browser, onLoad
-    // could load a tempalte via AJAX (although would result in
-    // a very slow interface before all tempaltes had been cached).
     Backbone.Marionette.Renderer.render = function (template, data) {
         var html;
-        // Callback will run sync if templates pre-compiled into dust cache
+        // Template must be compiled and in the dust cache. Recommend pre-compiling
+        // and loading the templates as scripts at app start.
         dust.render(template, data, function (err, out) {
             html = out;
         });
